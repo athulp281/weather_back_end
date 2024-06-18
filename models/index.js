010ec7bc965cec7cdba29fa606874a00d37d26db
athul <athulpranoor@gmail.com>
2024-06-18 09:41:47 +0530

    update 2

--- models/index.js
+++ models/index.js
@@ -7,8 +7,6 @@ const sequelize = new Sequelize(config.development);
 const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
-console.log("__dirname:", __dirname); // Log current directory
-console.log("Resolved path:", path.resolve(__dirname, "./globalnews")); // Log resolved path
 
 db.user = require("./user")(sequelize, Sequelize);
 // db.globalnews = require("./globalnews")(sequelize, Sequelize);
